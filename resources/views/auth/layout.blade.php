2fcd7bbd1edfd5137835bd0bf6912683e4925834
George Muema <georgemuemah@gmail.com>
2022-05-20 13:27:51 +0300

    first commit

---
+++ resources/views/auth/layout.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html" />
+	<meta name="author" content="lolkittens" />
+
+	<title>Untitled 1</title>
+</head>
+
+<body>
+
+<div>
+@yield('content')
+</div>
+
+</body>
+</html>
